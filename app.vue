d15ba3d283bf2509b1ee895ffd218b8d99a337d2
chloelo <chloe.lo@ennowell.com>
2024-05-15 11:47:42 +0800

    feat: 🎸 install tailwindcss

--- app.vue
+++ app.vue
@@ -1,8 +1,7 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <!-- <NuxtWelcome /> -->
+    <h1 class="text-3xl font-bold underline text-red-500">Hello world!</h1>
   </div>
 </template>
-<script setup>
-
-</script>
+<script setup></script>
